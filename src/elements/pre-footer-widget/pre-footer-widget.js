56b19c962dc4477f3f61cf451744b06a036899b9
Tomas Caufriez <tomas.caufriez@bosa.fgov.be>
2025-04-02 08:54:25 +0200

    pre footer

--- src/elements/pre-footer-widget/pre-footer-widget.js
+++ src/elements/pre-footer-widget/pre-footer-widget.js
@@ -112,18 +112,21 @@ class SDGPreFooterWidget extends HTMLElement {
       super();
 
       this.attachShadow({ mode: 'open' });
-      this.isChat = 0;
-      this.isPhone = 0;
+      this.widgetId = 'widget';
+      this.isChat = false;
+      this.chatId = null;
+      this.isPhone = false;
       this.phoneNumber = null;
-      this.isContact = 0;
+      this.isContact = false;
       this.contactForm = null;
-      this.isAppointment = 0;
+      this.isAppointment = false;
       this.appointmentUrl = null;
-      this.text = null
+      this.infoText = null
+      this.isListeningQuery = false;
 
       this.loadTemplate()
          .then(() => {
-            this.getParams();
+            this.getAttributes();
             this.addLanguageContent();
             this.listenChatButton();
          })
@@ -133,24 +136,68 @@ class SDGPreFooterWidget extends HTMLElement {
       this.shadowRoot.innerHTML = template;
    }
 
-   getParams () {
+   getAttributes () {
+      // get attributes from the element 
+      /*
+         Attributes:
+         - chat: boolean                  // display chat button (default: false)
+         - chat-id: string                // chat widget id (default: null)
+         - phone: boolean                 // display phone button (default: false)
+         - phone-number: string           // phone number (default: null)
+         - contact: boolean               // display contact button (default: false)
+         - contact-form: string           // contact form url (default: null)
+         - appointment: boolean           // display appointment button (default: false)
+         - appointment-url: string        // appointment url (default: null)
+         - info-text: JSON                // display text (default: null)
+         - listening-query: boolean       // listen query params (default: false)
+      */
+
+      this.isChat = this.getAttribute('chat') !== null && this.getAttribute('chat') !== 'false';
+      this.chatId = this.getAttribute('chat-id') || null;
+      this.isPhone = this.getAttribute('phone') !== null && this.getAttribute('phone') !== 'false';
+      this.phoneNumber = this.getAttribute('phone-number') || null;
+      this.isContact = this.getAttribute('contact') !== null && this.getAttribute('contact') !== 'false';
+      this.contactForm = this.getAttribute('contact-form') || null;
+      this.isAppointment = this.getAttribute('appointment') !== null && this.getAttribute('appointment') !== 'false';
+      this.appointmentUrl = this.getAttribute('appointment-url') || null;
+      this.infoText = this.getAttribute('info-text') ? JSON.parse(this.getAttribute('info-text')) : null;
+      this.isListeningQuery = this.getAttribute('listening-query') !== null && this.getAttribute('listening-query') !== 'false';
+
+      // set attributes to the widget
+      if (this.isChat) { this.shadowRoot.querySelector(`#${this.widgetId}`).classList.add('is-chat') }
+      if (this.isPhone) { this.shadowRoot.querySelector(`#${this.widgetId}`).classList.add('is-phone') }
+      if (this.isContact) { this.shadowRoot.querySelector(`#${this.widgetId}`).classList.add('is-contact') }
+      if (this.isAppointment) { this.shadowRoot.querySelector(`#${this.widgetId}`).classList.add('is-appointment') }
+      if (this.infoText) {
+         const lang = document.documentElement.lang || 'en';
+         this.shadowRoot.querySelector(`#${this.widgetId} #info-text`).innerHTML = this.infoText[lang]
+      }
+
+      // listening query
+      if (this.isListeningQuery) {
+         this.listenQuery();
+      }
+   }
+
+   listenQuery () {
       /*
          Params:
          - sdg-pf-chat=0              // display chat button (default: 0)
          - sdg-pf-phone=0             // display phone button (default: 0)
          - sdg-pf-phone-number=...    // phone number (default: null)
          - sdg-pf-contact=0           // display contact button (default: 0)
-         - sdg-pf-contact-url=...      // contact form url (default: null)
+         - sdg-pf-contact-url=...     // contact form url (default: null)
          - sdg-pf-appointment=0       // display appointment button (default: 0)
          - sdg-pf-appointment-url=... // appointment url (default: null)
          - sdg-pf-text                // display text (default: 0)
 
          example:
-         /demo/pre-footer-widget.html?sdg-feed-min=0&sdg-feed-rating=1&sdg-feed-text=1&sdg-feed-open=1&sdg-feed-overlay-click=0&sdg-button-id=fb4761e7-0ee2-48fd-89f3-ae7950f1c946
+         /demo/pre-footer-widget.html?sdg-feed-min=0&sdg-feed-rating=1&sdg-feed-text=1&sdg-feed-open=1&sdg-feed-overlay-close=0&sdg-button-id=fb4761e7-0ee2-48fd-89f3-ae7950f1c946
       */
       const urlParams = new URLSearchParams(window.location.search);
 
       this.isChat = urlParams.has('sdg-pf-chat') && urlParams.get('sdg-pf-chat') === '1';
+      this.chatId = urlParams.has('sdg-pf-chat-id') ? urlParams.get('sdg-pf-chat-id') : null;
       this.isPhone = urlParams.has('sdg-pf-phone') && urlParams.get('sdg-pf-phone') === '1';
       this.phoneNumber = urlParams.has('sdg-pf-phone-number') ? urlParams.get('sdg-pf-phone-number') : null;
       this.isContact = urlParams.has('sdg-pf-contact') && urlParams.get('sdg-pf-contact') === '1';
@@ -158,12 +205,6 @@ class SDGPreFooterWidget extends HTMLElement {
       this.isAppointment = urlParams.has('sdg-pf-appointment') && urlParams.get('sdg-pf-appointment') === '1';
       this.appointmentUrl = urlParams.has('sdg-pf-appointment-url') ? urlParams.get('sdg-pf-appointment-url') : null;
       this.text = urlParams.has('sdg-pf-text') ? urlParams.get('sdg-pf-text') : null;
-
-      // add class to the widget
-      // minized
-      if (this.isMinified) {
-         this.shadowRoot.querySelector('#widget').classList.add('minimized');
-      }
    }
 
    addLanguageContent () {
@@ -180,32 +221,45 @@ class SDGPreFooterWidget extends HTMLElement {
       // Get the website's language
       const locale = locales[lang] || locales['en'];
       // Set content to current language
-      if (headerTitle && chatButton && phoneButton && emailButton && appointmentButton && officeButton && info) {
+      if (headerTitle) {
          headerTitle.textContent = locale.title;
+      }
+
+      if (chatButton) {
          chatButton.querySelector('.title').textContent = locale.buttons.chat.title;
          chatButton.querySelector('.description').textContent = locale.buttons.chat.text;
+      }
+
+      if (phoneButton) {
          phoneButton.querySelector('.title').textContent = locale.buttons.phone.title;
          phoneButton.querySelector('.description').textContent = locale.buttons.phone.text;
+      }
+
+      if (emailButton) {
          emailButton.querySelector('.title').textContent = locale.buttons.email.title;
          emailButton.querySelector('.description').textContent = locale.buttons.email.text;
+      }
+
+      if (appointmentButton) {
          appointmentButton.querySelector('.title').textContent = locale.buttons.appointment.title;
          appointmentButton.querySelector('.description').textContent = locale.buttons.appointment.text;
+      }
+
+      if (officeButton) {
          officeButton.querySelector('.title').textContent = locale.buttons.office.title;
          officeButton.querySelector('.description').textContent = locale.buttons.office.text;
-         info.textContent = locale.info;
+      }
 
-         // hide office-button
-         officeButton.style.display = 'none';
+      if (info) {
+         info.textContent = locale.info;
       }
    }
 
    listenChatButton () {
       const chatButton = this.shadowRoot.querySelector('#chat-button');
       chatButton.addEventListener('click', () => {
-         console.log('chat');
-
          // Open feedback-widget
-         const feedbackWidget = document.querySelector('feedback-widget');
+         const feedbackWidget = this.chatId ? document.querySelector(`#${this.chatId}`) : document.querySelector('sdg-feedback-widget');
          if (feedbackWidget) {
             // remove closed class inside the feedback widget shadow root
             const feedbackWidgetShadowRoot = feedbackWidget.shadowRoot;
@@ -214,7 +268,7 @@ class SDGPreFooterWidget extends HTMLElement {
                closedClass.classList.remove('closed');
             }
             // open the feedback widget
-            // feedbackWidget.open();
+            feedbackWidget.open();
          } else {
             console.error('Feedback widget not found');
          }
